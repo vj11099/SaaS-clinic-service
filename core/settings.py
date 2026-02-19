0ed7b46d6bb2d0a32adb3cce28c08214b0612c8f
vj11099 <vishvamjoshi@gmail.com>
2026-02-19 12:37:34 +0530

    config changes

--- core/settings.py
+++ core/settings.py
@@ -278,10 +278,10 @@ SIMPLE_JWT = {
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'vishvamjoshi11099@gmail.com'
+# EMAIL_HOST_USER = os.getenv('EMAIL_ADDRESS')
 # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
-DEFAULT_FROM_EMAIL = 'noreply@yourapp.com'
+DEFAULT_FROM_EMAIL = os.getenv('SUBSCRIPTION_NOTIFICATION_EMAIL')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
